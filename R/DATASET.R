968241f5f95a4b1c205676cce50b93e57294ef2e
mohrosidi <48061153+mohrosidi@users.noreply.github.com>
2019-10-30 11:41:51 +0700

    Add new dataset (saddle_river, conecuh_river, and potomac river).

---
+++ R/DATASET.R
@@ -0,0 +1,35 @@
+#' @title Saddle River Discharge
+#' @description Annual peak discharges for the Saddle River, NJ data.
+#' @format A tibble with 65 rows and 2 variables:
+#' \describe{
+#'   \item{\code{Year}}{year of discharges recording}
+#'   \item{\code{Flow}}{annual peak discharges, in cfs}
+#'}
+#'@note The data come from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"saddle_river"
+
+#' @title Conecuh River Streamflows
+#' @description Annual streamflows for the Conecuh River, AL data.
+#' @format A tibble with 20 rows and 2 variables:
+#' \describe{
+#'   \item{\code{Year}}{year of streamflows recording}
+#'   \item{\code{Flow}}{annual streamflows, in cfs}
+#'}
+#'@note The data come from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"conecuh_river"
+
+#' @title Potomac River Streamflows
+#' @description Ranked daily streamflow for the Potomac River, Wash. D.C data.
+#' @format A tibble with 365 rows and 2 variables:
+#' \describe{
+#'   \item{\code{Year}}{year of streamflows recording}
+#'   \item{\code{Flow}}{daily streamflows, in cfs}
+#'}
+#'@note The data come from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"potomac_river"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data("DATASET")
+library(tidyverse)
+
+saddle_river <- read_excel("data-raw/hhappc.xls", sheet = "appc1")
+conecuh_river <- read_excel("data-raw/hhappc.xls", sheet = "appc2") %>%
+  select(Year = Year, Flow = `Flow (cfs)`)
+potomac_river <- read_excel("data-raw/hhappc.xls", sheet = "appc3")
+
+usethis::use_data(saddle_river,
+                  conecuh_river,
+                  potomac_river,
+                  overwrite = TRUE)
