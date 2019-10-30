10400b22c23223f858049117173267dad43ea410
mohrosidi <48061153+mohrosidi@users.noreply.github.com>
2019-10-30 23:12:13 +0700

    Add new dataset to package

--- R/DATASET.R
+++ R/DATASET.R
@@ -98,7 +98,7 @@
 #' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
 "corbicula"
 
-#' @title Total Disolved Solid
+#' @title Total Dissolved Solid
 #' @description TDS concentrations for the Cuyahoga River, Ohio
 #' @format A tibble with 80 rows and 4 variables:
 #' \describe{
@@ -125,3 +125,147 @@
 #'DR.Helsel and R.M. Hirsch (2002).
 #' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
 "phosphorus"
+
+#' @title Alluvial Aquifer
+#' @description Grain size and permeability of alluvial aquifers
+#' @format A tibble with 59 rows and 2 variables:
+#' \describe{
+#'   \item{\code{Median_grain_size}}{median grain size particle, in mm}
+#'   \item{\code{Yield}}{groundwater yield, in \eqn{gal/day/ft^{2}}}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"alluvial"
+
+#' @title ROE and TDS Data
+#' @description ROE and TDS data, Rappahannock R. near Fredricksburg, VA.
+#' @format A tibble with 84 rows and 3 variables:
+#' \describe{
+#'   \item{\code{ROE}}{residue on evaporation}
+#'   \item{\code{TDS}}{total dissolved solid, in ppm}
+#'   \item{\code{Date}}{date of measurement}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"roe"
+
+#' @title Streamflow Record
+#' @description Streamflow data used for record extension
+#' @format A tibble with 50 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Year}}{year of measurement}
+#'   \item{\code{Base_sf}}{base streamflow, in million cubic meters per month}
+#'   \item{\code{Short_sf}}{short streamflow, in million cubic meters per month}
+#'   \item{\code{Actual_early_sf}}{actual early streamflow at short, in million cubic meters per month}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"sf_record"
+
+#' @title Runoff and Basin Characteristic
+#' @description Mean annual runoff and basin characteristics
+#' @format A tibble with 13 rows and 10 variables:
+#' \describe{
+#'   \item{\code{ROFF}}{mean annual runoff}
+#'   \item{\code{PRECIP}}{precipitation}
+#'   \item{\code{AREA}}{drainage area}
+#'   \item{\code{SLOPE}}{average slope}
+#'   \item{\code{LENGTH}}{the length of the drainage basin}
+#'   \item{\code{PERIM}}{the perimeter of the basin}
+#'   \item{\code{DI}}{the diameter of the largest circle which could be inscribed within the drainage basin }
+#'   \item{\code{Rs}}{shape factor}
+#'   \item{\code{FREQ}}{the stream frequency -- the ratio of the number of streams in the basin to the basin area}
+#'   \item{\code{Rr}}{ relief ratio}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"runbas"
+
+#' @title Urban Total Nitrogen Loads
+#' @description Data from 42 small urban drainage basins located in several cities around United States.
+#' @format A tibble with 42 rows and 9 variables:
+#' \describe{
+#'   \item{\code{LOGTN}}{log total nitrogen load}
+#'   \item{\code{LOGDA}}{log contributing area}
+#'   \item{\code{LOGIMP}}{log impervious area}
+#'   \item{\code{MJTEMP}}{mean minimum January temperature}
+#'   \item{\code{MSRAIN}}{mean seasonal rainfall}
+#'   \item{\code{PRES}}{percentage of area residential}
+#'   \item{\code{PNON}}{percentage of area non-urban}
+#'   \item{\code{PCOMM}}{percentage of area commercial}
+#'   \item{\code{PIND}}{percentage of area industrial}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"totalN"
+
+#' @title Uranium and TDS in Groundwaters
+#' @description Uranium and TDS concentration in Ogallala aquifer.
+#' @format A tibble with 44 rows and 3 variables:
+#' \describe{
+#'   \item{\code{TDS}}{tds concentration}
+#'   \item{\code{Uranium}}{uranium concentration}
+#'   \item{\code{Bicarbonate}}{bicarbonate concentration, `0 <= 50% and 1 > 50%'}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"utds"
+
+#' @title Sediment Transport
+#' @description Green River, Kentucky sediment transport data.
+#' @format A tibble with 21 rows and 3 variables:
+#' \describe{
+#'   \item{\code{Year}}{year of measurement}
+#'   \item{\code{Load}}{load of sediment}
+#'   \item{\code{Q}}{stream discharge}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"sediment"
+
+#' @title Total P Trends Data
+#' @description Maumee River, Ohio total P trends data
+#' @format A tibble with 133 rows and 3 variables:
+#' \describe{
+#'   \item{\code{Date}}{date of measurement}
+#'   \item{\code{Discharge}}{stream discharge, in 1000 cfs}
+#'   \item{\code{Load}}{total P load, in tons/day}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"totalP"
+
+#' @title Water Levels Data
+#' @description Water levels, P-R-M system middle aquifer, NJ.
+#' @format A tibble with 90 rows and 2 variables:
+#' \describe{
+#'   \item{\code{Date}}{date of measurement}
+#'   \item{\code{Elevation}}{elevation, in ft}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"water_level"
+
+#' @title Contamination Rating
+#' @description Factors affecting contamination from impoundments
+#' @format A tibble with 124 rows and 5 variables:
+#' \describe{
+#'   \item{\code{UZT}}{unsaturated zone thickness, possible scores: 0 (favorable) to 9 (unfavorable)}
+#'   \item{\code{AY}}{yields: aquifer properties, possible scores: 0 (poor) to 6 (good)}
+#'   \item{\code{GWQ}}{groundwater quality, possible scores: 0 (poor) to 5 (excellent)}
+#'   \item{\code{HWM}}{hazard rating for source, possible scores: 1 (low) to 9 (high)}
+#'   \item{\code{CR}}{contamination rating, possible scores: 0 (contaminated) to 1 (not contaminated)}
+#'}
+#' @note The data comes from Statistical Methods for Water Resources Book written by:
+#'DR.Helsel and R.M. Hirsch (2002).
+#' @source \url{http://water.usgs.gov/pubs/twri/twri4a3/}
+"contamination"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -32,14 +32,47 @@ tds <- read_excel("data-raw/hhappc.xls", sheet = "appc9") %>%
 
 phosphorus <- read_excel("data-raw/hhappc.xls", sheet = "appc10")
 
-usethis::use_data(saddle_river,
-                  conecuh_river,
-                  potomac_river,
-                  atrazine,
-                  iron,
-                  iron2,
-                  scwell,
-                  corbicula,
-                  tds,
-                  phosphorus,
+alluvial <- read_excel("data-raw/hhappc.xls", sheet = "appc11") %>%
+  select(Median_grain_size = `Median grain size`, Yield)
+
+roe <- read_excel("data-raw/hhappc.xls", sheet = "appc12") %>%
+  mutate(Year = 19) %>%
+  separate(col = `YYMMDD Date`, into = c("YY", "MM", "DD"), sep="-", convert = TRUE) %>%
+  unite(Year, YY , col="YY", sep="") %>%
+  unite(YY, MM, DD, col="Date", sep="-") %>%
+  mutate(Date = as.Date(Date))
+
+sf_record <- read_excel("data-raw/hhappc.xls", sheet = "appc13") %>%
+  select(Year, Base_sf = `Base streamflow`, Short_sf = `Short streamflow`,
+         Actual_early_sf = `actual early streamflow at short`)
+
+runbas <- read_excel("data-raw/hhappc.xls", sheet = "appc14")
+
+totalN <- read_excel("data-raw/hhappc.xls", sheet = "appc15")
+
+utds <- read_excel("data-raw/hhappc.xls", sheet = "appc16")
+
+sediment <- read_excel("data-raw/hhappc.xls", sheet = "appc17")
+
+totalP <- read_excel("data-raw/hhappc.xls", sheet = "appc18") %>%
+  select(Year = Dectime, Month, Discharge = `Discharge (1000cfs)`,
+         Load = `Load (tons/day)`) %>%
+  mutate(Year = floor(Year), Date = 1) %>%
+  unite(Year, Month, Date, col="Date", sep="-") %>%
+  mutate(Date = as.Date(Date))
+
+water_level <- read_excel("data-raw/hhappc.xls", sheet = "appc19") %>%
+  select(Day, Month, Year, Elevation) %>%
+  unite(Year, Month, Day, col="Date", sep="-") %>%
+  mutate(Date = as.Date(Date))
+
+contamination <- read_excel("data-raw/hhappc.xls", sheet = "appc20") %>%
+  select(UZT = `Unsat Zone Thickness`, AY = `Aquifer yields`,
+         GWQ = `Ground Water Quality`, HWM = `Hazard of Waste Mtl`,
+         CR = `Contamination Rating`)
+
+usethis::use_data(saddle_river, conecuh_river, potomac_river, atrazine,
+                  iron, iron2, scwell, corbicula, tds, phosphorus,
+                  alluvial, roe, sf_record, runbas, totalN, utds,
+                  sediment, totalP, water_level,contamination,
                   overwrite = TRUE)
